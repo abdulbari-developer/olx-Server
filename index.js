8a01a190a2f7cac8ae583cbb3b27b47b3395756c
abdulbari-developer <your.email@example.com>
2025-10-20 20:55:46 +0500

    ;

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ app.use(cors({
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }))
-app.options('*', cors());
+// app.options('*', cors());
 app.use(cookieParser())
 app.use(express.json())
 
